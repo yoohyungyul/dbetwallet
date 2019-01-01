5d9b67072f10e66a62699d909a8fbba2f4ea8cd6
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 15:16:26 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -1,1 +1,21 @@
-wallet main
+@extends('v2.layouts.layout_etc')
+
+@section('title', 'DBET Wallet')
+
+
+@section('style')
+
+@endsection
+
+@section('content')
+
+
+@endsection
+
+
+@section('script')
+<script>
+    
+
+</script>
+@endsection
