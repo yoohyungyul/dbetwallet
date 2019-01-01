cc3dc908e61027b7bcd3e7dcc27b00c6c2a6242c
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 14:47:41 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -9,6 +9,8 @@ use App\Http\Requests;
 class WalletController extends Controller
 {
     public function getWallet() {
-        return view('wallet.wallet');
+        echo "dd";
+        exit;
+        //return view('wallet.wallet');
     }
 }
