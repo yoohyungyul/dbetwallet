d958b5bc7d778d9f1ddfff29e0aa2bd8971a3d9d
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 15:32:32 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -8,20 +8,24 @@
 @endsection
 
 @section('content')
-<ul class="nav nav-pills nav-fill">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Longer nav link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </li>
-</ul>
+<div class="row">
+    <div class="col-md-12">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Longer nav link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</div>
 
 
 @endsection
