b53e1befc71f15086fb1a635638163c2105c1ca4
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 15:30:01 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -8,6 +8,20 @@
 @endsection
 
 @section('content')
+<ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Longer nav link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+</ul>
 
 
 @endsection
