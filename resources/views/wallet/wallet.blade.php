1bf75d509ded2ea9665a6becad4aa57ef170dd41
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 15:37:03 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -12,16 +12,13 @@
     <div class="col-md-12">
         <ul class="nav nav-pills nav-fill">
             <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
+                <a class="nav-link active" href="#">Wallet</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Longer nav link</a>
+                <a class="nav-link" href="#">History</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link" href="#">Send</a>
             </li>
         </ul>
     </div>
