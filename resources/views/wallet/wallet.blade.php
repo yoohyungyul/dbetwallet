1da92ac26a48b7f15f5834ebd5abe11af97ab824
yuhyeong-yeol <chemical91@nate.com>
2019-01-01 16:14:11 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -38,8 +38,8 @@
 </div>
 <div class="row mt40">
     <div class="col-12 text-center">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card" >
+            <img src="..." class="card-img-top" alt="QR Code">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
