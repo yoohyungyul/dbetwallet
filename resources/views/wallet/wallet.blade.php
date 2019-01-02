5fa7c35368140b6d16bf7a8aac5bb42f0915b7fd
유형열 <>
2019-01-02 11:03:32 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -30,7 +30,7 @@
         <div class="card text-center" style="display: block">
             <img src="/img/qrcode.png" class="card-img-top pt20" alt="QR Code">
             <div class="card-body text-left">
-                <span style="float: right;">복사</span>
+                <span style="float: right;"><i class="fa fa-files-o"></i></span>
                 <h5 class="card-title">Address
                     
                 </h5>
