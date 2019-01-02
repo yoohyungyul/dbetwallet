d802d8f9c9ef31de37b80519b9bfbccb8ac8ef01
유형열 <>
2019-01-02 10:10:36 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -10,34 +10,20 @@
 @section('content')
 <div class="row  mt20">
     <div class="col-12">
-        <!-- <ul class="nav nav-pills nav-fill">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Wallet</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">History</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Send</a>
-            </li>
-        </ul> -->
-        <nav>
-        <div class="nav nav-tabs nav-fill" >
-            <a class="nav-item nav-link active" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-            <a class="nav-item nav-link"  data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-            <a class="nav-item nav-link"  data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
-        </div>
+       <nav>
+            <div class="nav nav-tabs nav-fill" >
+                <a class="nav-item nav-link active"  href="/wallet" >Wallet</a>
+                <a class="nav-item nav-link" href="/history" >History</a>
+                <a class="nav-item nav-link"   href="/send" >Send</a>
+            </div>
         </nav>
-       
+        <div class="tab-content" >
+            <span style="float: right;display: inline-block;padding:0 10px 0 10px">DBET</span>
+            <span style="float: right;">100,000,000</span>
+       </div>
     </div>
 </div>
-<div class="row  mt20">
-    <div class="col-12 text-right">
-        
-        <span style="float: right;display: inline-block;padding:0 10px 0 10px">DBET</span>
-        <span style="float: right;">100,000,000</span>
-    </div>
-</div>
+
 <hr>
 <div class="row mt40">
     <div class="col-12 text-center">
