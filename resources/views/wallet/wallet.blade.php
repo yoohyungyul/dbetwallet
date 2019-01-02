0d3c5b81c848dc475df25c5a164313a1e4d60067
유형열 <>
2019-01-02 10:04:25 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <div class="row  mt20">
     <div class="col-12">
-        <ul class="nav nav-pills nav-fill">
+        <!-- <ul class="nav nav-pills nav-fill">
             <li class="nav-item">
                 <a class="nav-link active" href="#">Wallet</a>
             </li>
@@ -20,7 +20,19 @@
             <li class="nav-item">
                 <a class="nav-link" href="#">Send</a>
             </li>
-        </ul>
+        </ul> -->
+        <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+        </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+        </div>
     </div>
 </div>
 <div class="row  mt20">
