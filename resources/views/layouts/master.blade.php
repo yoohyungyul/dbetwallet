13beee1af0d8c1adac83cbbd92c7451500becd64
유형열 <>
2019-01-02 12:39:37 +0900

    no message

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,12 +25,12 @@
     <body>
         
         <nav class="navbar navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">DBET Wallet</a>
+            <a class="navbar-brand" href="/">DBET Wallet</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
 
                 @if (Auth::guest())
@@ -52,7 +52,7 @@
                 @endif
 
 
-            </div>
+            </div> -->
         </nav>
     
         <div class="container">
