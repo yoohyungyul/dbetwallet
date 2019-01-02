92296187932acb8488dad7caacbfbd857d59d773
유형열 <>
2019-01-02 11:23:28 +0900

    no message

--- resources/views/wallet/history.blade.php
+++ resources/views/wallet/history.blade.php
@@ -31,29 +31,17 @@
                     <thead>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Txid</th>
+                        <th scope="col">수량</th>
+                        <th scope="col">상세보기</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                         <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                        <td>0xjfoekjdijfkjiefjildjkjfiefkdf</td>
+                        <td>1.000000</td>
+                        <td><button type="button" class="btn">보기</button></td>
                         </tr>
                     </tbody>
                 </table>
