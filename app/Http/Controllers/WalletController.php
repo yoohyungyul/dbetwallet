2ff258ac9df6ce950448fbff22fb1d42582d8c80
유형열 <>
2019-01-04 17:13:20 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -15,7 +15,7 @@ class WalletController extends Controller
     // 지갑 
     public function getWallet() {
 
-        $client = new jsonRPCClient("172.31.20.32", "9101");
+        $client = new jsonRPCClient("172.31.20.32", "9100");
     	$result = $client->request('personal_newAccount', "123456");
     		
         print_R($result);
