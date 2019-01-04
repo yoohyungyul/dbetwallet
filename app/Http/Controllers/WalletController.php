e590d22f6913208aea3cae298111b32361235762
유형열 <>
2019-01-04 17:38:12 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -15,7 +15,7 @@ class WalletController extends Controller
     // 지갑 
     public function getWallet() {
 
-        $client = new jsonRPCClient("172.31.20.32", "9100");
+        $client = new jsonRPCClient("172.31.20.32", "9101");
     	$result = $client->request('personal_newAccount', "123456");
     		
         print_R($result);
@@ -54,3 +54,11 @@ class WalletController extends Controller
 
 // chainplus_5c2c60134e86b9.78143218
 // 33자리
+
+/*
+curl --data '{"method":"eth_accounts","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST 54.180.124.202:9101
+
+
+curl --data '{"method":"personal_newAccount","params":["123456"],"id":2,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST 54.180.124.202:9101
+curl --data '{"method":"personal_newAccount","params":["MelonBIT@Master-Wallet#000001"],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:9101
+*/
