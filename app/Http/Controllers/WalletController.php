5d9ac13b2af81581b164ab9ceb33a71bfc15404d
유형열 <>
2019-01-07 15:25:13 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -64,7 +64,7 @@ class WalletController extends Controller
             $real_to = str_pad(str_replace('0x','',$to), 64, '0', STR_PAD_LEFT);
             $real_amount = str_pad(dechex(($amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
             
-            $result = $client->request('personal_unlockAccount', ["0x007bB2cb9e1e9B7a4aFB55332DDbD78E7b1611EC", "123456", '0x0a']);
+            $result = $client->request('personal_unlockAccount', ["0x8ec6ae654aa5483c506bd833d814fb6aae3241f9", "123456", '0x0a']);
             print_R($result);
             exit;
 
