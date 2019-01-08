763e64baea83e93aebdefe10f440a95481b04565
유형열 <>
2019-01-08 17:33:23 +0900

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,8 +19,8 @@
 Route::group(['middleware' => 'csrf', "middleware" => "my_currency"], function($router)
 {
 
-    // Route::auth();
-    Route::get('login', 'Auth\AuthController@getLogin');
+    Route::auth();
+    // Route::get('login', 'Auth\AuthController@getLogin');
     // Route::post('login', 'Auth\AuthController@postLogin');
 
     // Route::post('login', ['middleware' => 'GrahamCampbell\Throttle\Http\Middleware\ThrottleMiddleware:5,1', 'uses' => 'Auth\AuthController@postLogin']);
