12cb700621b48fa633c79ab536cc3fce5196cf57
유형열 <>
2019-01-08 14:28:42 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -17,6 +17,10 @@ class WalletController extends Controller
     // 지갑 
     public function getWallet() {
 
+
+        Cookie::forget('chaninplus');
+
+
         // echo Cookie::get('chaninplus');
         return view('wallet.wallet');
 
