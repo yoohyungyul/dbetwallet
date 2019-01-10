fdc9bc16b5bb033a1b8ece4d7f1a846c0b97b574
유형열 <>
2019-01-10 15:29:53 +0900

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 <html>
 <head>
 <title>..</title>
-<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr"> 
 <script language="JavaScript">
 function nextWin()
 {location = "/wallet"}
-</script>
-</head>
-<body omLoad="setTimeout('nextWin()', 3000)"> <!--1000 이 1초 입니다.-->
+</script> 
+</head> 
+<body onLoad="setTimeout('nextWin()', 1000)"> <!--1000 이 1초 입니다.-->
 바로 이동한 원하는 사이트로 이동함
 </body>
 </html>
