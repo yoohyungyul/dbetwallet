feb72b66eeb0ba30591163ef6d3680ea6ec5c078
유형열 <>
2019-01-10 16:15:09 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default" >
                 <div class="panel-body mt70" style="border:1px solid #ff00ff">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                    <form  role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
@@ -32,7 +32,7 @@
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-4">
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
