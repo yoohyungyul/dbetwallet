ad6da168ce58a60c2bef6fe48cfe83f7200311df
유형열 <>
2019-01-10 16:18:20 +0900

    no message

--- resources/views/2fa/login.blade.php
+++ resources/views/2fa/login.blade.php
@@ -10,16 +10,16 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-8 col-offset-2">
+        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-body mt70">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/2fa/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-6">
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email">
 
                                
@@ -28,16 +28,16 @@
 
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-4 control-label">OTP CODE</label>
+                            <label for="name" class="col-md-4 control-label">OTP CODE</label>
 
-                            <div class="col-6">
+                            <div class="col-md-6">
                                 <input id="totp" type="text" class="form-control" name="totp" value="" maxlength="6">
 
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-5">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Login
                                 </button>
