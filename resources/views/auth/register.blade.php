a0f45255d162e3f33277852b4867c7afc39d14ca
유형열 <>
2019-01-10 15:37:36 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,12 +44,12 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6">
+                            <div class="col-6">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
                                 </button>
                             </div>
-                            <div class="col-md-6 text-right">
+                            <div class="col-6 text-right">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
                                 </button>
