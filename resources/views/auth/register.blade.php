482eec1141bcd788d279d3c4221bf090200c2ff7
유형열 <>
2019-01-10 15:39:24 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-4 control-label">Name</label>
 
                             <div class="col-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
@@ -30,7 +30,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-4 control-label">E-Mail Address</label>
 
                             <div class="col-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -44,12 +44,12 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-6">
+                            <div class="col-5">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
                                 </button>
                             </div>
-                            <div class="col-6 text-right">
+                            <div class="col-5 text-right">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
                                 </button>
