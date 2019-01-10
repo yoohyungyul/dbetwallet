2df16b5b294328e2567f93b7ef0551ef2f04be63
유형열 <>
2019-01-10 16:10:51 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,9 +16,9 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-4 control-label">Name</label>
+                            <label for="name" class="col-sm-4 control-label">Name</label>
 
-                            <div class="col-6">
+                            <div class="col-sm-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
 
                                 @if ($errors->has('name'))
