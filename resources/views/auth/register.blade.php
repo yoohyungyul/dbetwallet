cdd6bb7f6cb32e687b41de2971cecf98eb5c62e4
유형열 <>
2019-01-10 15:38:12 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-8 col-offset-2">
             <div class="panel panel-default">
                 <div class="panel-body mt70">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
