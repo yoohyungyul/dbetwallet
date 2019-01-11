1a91ab6db9e1db455aa004a490b7e09daed02225
유형열 <>
2019-01-11 10:17:05 +0900

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,23 @@
 <html>
 <head>
-<title>..</title>
+<title>DBET Wallet</title>
 <meta http-equiv="Content-Type" content="text/html; charset=euc-kr"> 
-<script language="JavaScript">
-function nextWin()
-{location = "/wallet"}
-</script> 
-<style>
+    <script language="JavaScript">
+    function nextWin()
+    {location = "/wallet"}
+    </script> 
+    <style>
 
-.layer{position: absolute; top: 0; left: 0; width: 100%; height: 100%; text-align: center}
-.layer .content{display: inline-block; vertical-align: middle}
-.layer .blank{display: inline-block; width: 0; height: 100%; vertical-align: middle}
+    .layer{position: absolute; top: 0; left: 0; width: 100%; height: 100%; text-align: center}
+    .layer .content{display: inline-block; vertical-align: middle}
+    .layer .blank{display: inline-block; width: 0; height: 100%; vertical-align: middle}
 
-</style>
+    </style>
 </head> 
 <body onLoad="setTimeout('nextWin()', 3000)"> <!--1000 이 1초 입니다.-->
 <div class="layer">
-
     <span class="content">로딩...</span>
-
     <span class="blank"></span>
-
 </div>
 </body>
 </html>
