9bced74c86bbb537e9f6d96aab21ae84443dbecc
유형열 <>
2019-01-11 09:55:47 +0900

    no message

--- resources/views/wallet/send.blade.php
+++ resources/views/wallet/send.blade.php
@@ -48,16 +48,16 @@
                 </form>
 
 
-                <input type=text size=16 placeholder="Tracking Code" class=qrcode-text><label class=qrcode-text-btn><input type=file accept="image/*" capture=environment onchange="openQRCamera(this);" tabindex=-1></label> 
-<input type=button value="Go" disabled>
+                <!--input type=text size=16 placeholder="Tracking Code" class=qrcode-text><label class=qrcode-text-btn><input type=file accept="image/*" capture=environment onchange="openQRCamera(this);" tabindex=-1></label> 
+                <input type=button value="Go" disabled>
 
-<style>
-body, input {font-size:14pt}
-input, label {vertical-align:middle}
-.qrcode-text {padding-right:1.7em; margin-right:0}
-.qrcode-text-btn {display:inline-block; background:url(//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/07/1499401426qr_icon.svg) 50% 50% no-repeat; height:1em; width:1.7em; margin-left:-1.7em; cursor:pointer}
-.qrcode-text-btn > input[type=file] {position:absolute; overflow:hidden; width:1px; height:1px; opacity:0}
-</style>
+                <style>
+                body, input {font-size:14pt}
+                input, label {vertical-align:middle}
+                .qrcode-text {padding-right:1.7em; margin-right:0}
+                .qrcode-text-btn {display:inline-block; background:url(//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/07/1499401426qr_icon.svg) 50% 50% no-repeat; height:1em; width:1.7em; margin-left:-1.7em; cursor:pointer}
+                .qrcode-text-btn > input[type=file] {position:absolute; overflow:hidden; width:1px; height:1px; opacity:0}
+                </style-->
 
 
             </div>
@@ -109,7 +109,7 @@ input, label {vertical-align:middle}
 
         return true;
     }
-
+    /*
     function openQRCamera(node) {
         var reader = new FileReader();
         reader.onload = function() {
@@ -125,5 +125,6 @@ input, label {vertical-align:middle}
         };
         reader.readAsDataURL(node.files[0]);
     }
+    */
 </script>
 @endsection
