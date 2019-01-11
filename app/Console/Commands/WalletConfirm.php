16e5a1c1397d3bdfedb81d3fb2650a9e0683f2c5
유형열 <>
2019-01-11 16:38:01 +0900

    no message

--- app/Console/Commands/WalletConfirm.php
+++ app/Console/Commands/WalletConfirm.php
@@ -9,6 +9,8 @@ use Log;
 use App\TransactionHistory;
 use App\Currency;
 use App\jsonRPCClient;
+use App\Users_wallet;
+use App\Balance;
 
 class WalletConfirm extends Command {
 
@@ -77,23 +79,62 @@ class WalletConfirm extends Command {
 
                         
                         if(hexdec($current_block) - hexdec($result->result->blockNumber) > $history->confirm) {
-                            echo " send Complete!";
 
-                            $history->confirm = hexdec($current_block) - hexdec($result->result->blockNumber);
-                            $history->state = 1;
-                            $history->save();
 
-                            // 받는 사람 주소를 조회 후 있으면 등록 
-                            // 히스토리 등록
-                            // 발란스 수정
+                            try {
+                                DB::beginTransaction();
+                            
+
+                                $history->confirm = hexdec($current_block) - hexdec($result->result->blockNumber);
+                                $history->state = 1;
+                                $history->save();
+
+                                // 받는 사람 주소를 조회 후 있으면 등록 
+                                $to_userid = Users_wallet::where('address',$history->address->to)->value('user_id');
+                                if($to_userid) {
+                                    // 받는 사람 발란스 가져오기
+                                    $to_user_balance = Balance::where('user_id',$to_userid)->where('currency_id',env('CURRENCY_ID', '1'))->first();
+
+                                    // 발란스 업데이트
+                                    $to_user_balance->balance += $history->amount;
+                                    $to_user_balance->save();
+                                    
+                                    // 히스트로 등록
+                                    $transaction_history = new TransactionHistory;
+                                    $transaction_history->type = 2;
+                                    $transaction_history->user_id = $to_userid;
+                                    $transaction_history->currency_id = env('CURRENCY_ID', '1');
+                                    $transaction_history->amount = $history->amount;
+                                    $transaction_history->balance = $to_user_balance->balance;
+                                    $transaction_history->txid = $history->txid;
+                                    $transaction_history->address_from = $history->address_from;
+                                    $transaction_history->address_to = $history->address_to;
+                                    $transaction_history->state = $history->state;
+                                    $transaction_history->confirm = $history->confirm;
+                                    $transaction_history->push();
+
+                                } else {
+                                    echo "No User Address";
+                                }
+
+                            } catch (\Exception $e) {
+                                DB::rollback();
+
+                                echo " DB Error ";
+                            } finally {
+                                DB::commit();
+
+                                echo " send Complete!";
+                            }
 
                             
                         } else {
-                            echo " send Pending!";
+                            
                             
                             $history->history = hexdec($current_block) - hexdec($result->result->blockNumber);
-
                             $history->save();
+
+                            echo " send Pending!";
                         }
 
                     } else {
