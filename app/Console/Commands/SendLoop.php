b468eb3056b52403f8a0acf37a31e16ee4dbe0a0
유형열 <>
2019-01-11 14:52:59 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -49,6 +49,7 @@ class SendLoop extends Command
 
         $history = TransactionHistory::where('txid','')->where('state','0')->orderBy('id','asc')->get();
 
+        $funcs = "0xa9059cbb";
 
         foreach($history as $data) {
 
@@ -69,17 +70,17 @@ class SendLoop extends Command
 
 
             
-            $funcs = "0xa9059cbb";
+            
 
             $real_to = str_pad(str_replace('0x','',$data->address_to), 64, '0', STR_PAD_LEFT);
             $real_amount = str_pad($client->dec2hex(($data->amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
 
 
-            $result = $client->request('personal_unlockAccount', ["0x1b4906b8140114af27c306280981d5e251f5d072", "123456", '0x0a']);
+            $result = $client->request('personal_unlockAccount', [$currency->address, $currency->password, '0x0a']);
             
             $result = $client->request('eth_sendTransaction', [[
-                'from' => "0x1b4906b8140114af27c306280981d5e251f5d072",
-                'to' => "0x099606ECb05d7E94F88EFa700225880297dD55eF",
+                'from' => $currency->address,
+                'to' => $currency->contract,
                 'data' => $funcs.$real_to.$real_amount,
             ]]);
 
