725dea8d0eb412b294610fe186d7013caf961687
유형열 <>
2019-01-11 11:33:06 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -49,9 +49,14 @@ class SendLoop extends Command
 
         $history = TransactionHistory::where('txid','')->where('state','0')->orderBy('id','asc')->get();
 
-        echo "이더 : ".hexdec("0x640b5eece000")/pow(10,8);
-        
+        $client = new jsonRPCClient($currency->ip, $currency->port);
+
+        $result = $client->request('personal_unlockAccount', ["0x1b4906b8140114af27c306280981d5e251f5d072", "123456", '0x0a']);
         exit;
+
+        // echo "이더 : ".hexdec("0x640b5eece000")/pow(10,8);
+        
+        // exit;
 
         foreach($history as $data) {
 
@@ -67,23 +72,23 @@ class SendLoop extends Command
             // print_R($result);
 
               // 이더 조회
-            $result = $client->request('eth_getBalance', ["0x0D3183F579F6f5b28C60B09bD20A696bC80BF15b", 'latest']);
-            echo "이더 : ".hexdec($result->result)/pow(10,8);
+            // $result = $client->request('eth_getBalance', ["0x0D3183F579F6f5b28C60B09bD20A696bC80BF15b", 'latest']);
+            // echo "이더 : ".hexdec($result->result)/pow(10,8);
 
             // curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x1B4906B8140114aF27c306280981d5e251f5D072", "latest"],"id":1}' -H "Content-Type: application/json" -X POST localhost:9101
             
 
             // // 토큰 조회
-            $result = $client->request('eth_call', [[ 
-             "to" => "0x099606ECb05d7E94F88EFa700225880297dD55eF", 
-             "data" => "0x70a08231000000000000000000000000". str_replace("0x","","0x0D3183F579F6f5b28C60B09bD20A696bC80BF15b") ]]);
-            echo "토큰 : ".hexdec($result->result)/pow(10,8);
+            // $result = $client->request('eth_call', [[ 
+            //  "to" => "0x099606ECb05d7E94F88EFa700225880297dD55eF", 
+            //  "data" => "0x70a08231000000000000000000000000". str_replace("0x","","0x0D3183F579F6f5b28C60B09bD20A696bC80BF15b") ]]);
+            // echo "토큰 : ".hexdec($result->result)/pow(10,8);
 
 
             
-            // $result = $client->request('personal_unlockAccount', ["0x0d3183f579f6f5b28c60b09bd20a696bc80bf15b", "123456", '0x0a']);
+            $result = $client->request('personal_unlockAccount', ["0x1b4906b8140114af27c306280981d5e251f5d072", "123456", '0x0a']);
             // $result = $client->request('eth_sendTransaction', [[
-            //     'from' => "0x007bB2cb9e1e9B7a4aFB55332DDbD78E7b1611EC",
+            //     'from' => "0x1b4906b8140114af27c306280981d5e251f5d072",
             //     'to' => "0x099606ECb05d7E94F88EFa700225880297dD55eF",
             //     'data' => $funcs.$real_to.$real_amount,
             // ]]);
