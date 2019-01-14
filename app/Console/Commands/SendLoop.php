5e392e2ce67b44402cab3625d2932fa2b39e0d05
유형열 <>
2019-01-14 17:39:26 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -67,25 +67,17 @@ class SendLoop extends Command
             $result = $client->request('personal_unlockAccount', [$currency->address, $currency->password, '0x0a']);
 
     
+            $real_to = str_pad(str_replace('0x','',$data->address_to), 64, '0', STR_PAD_LEFT);
+            $real_amount = str_pad($client->dec2hex(($data->amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
+
+
+            $result1 = $client->request('personal_unlockAccount', [$currency->address, $currency->password, '0x0a']);
             
-            try {
-
-                $real_to = str_pad(str_replace('0x','',$data->address_to), 64, '0', STR_PAD_LEFT);
-                $real_amount = str_pad($client->dec2hex(($data->amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
-
-
-                $result1 = $client->request('personal_unlockAccount', [$currency->address, $currency->password, '0x0a']);
-                
-                $result = $client->request('eth_sendTransaction', [[
-                    'from' => $currency->address,
-                    'to' => $currency->contract,
-                    'data' => $funcs.$real_to.$real_amount,
-                ]]);
-            } catch(\Exception $e) {
-                $result = (object) [
-                    'result' => '',
-                ];
-            }
+            $result = $client->request('eth_sendTransaction', [[
+                'from' => $currency->address,
+                'to' => $currency->contract,
+                'data' => $funcs.$real_to.$real_amount,
+            ]]);
 
             print_R($result);
             if(is_object($result)) {
