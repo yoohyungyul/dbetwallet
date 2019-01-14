e6e38f7c203b40c48f55d6f5584e3ea2b5ad9abe
유형열 <>
2019-01-14 17:48:38 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -58,7 +58,7 @@ class SendLoop extends Command
 
 
         foreach($history as $data) {
-            
+
     
             $real_to = str_pad(str_replace('0x','',$data->address_to), 64, '0', STR_PAD_LEFT);
             $real_amount = str_pad($client->dec2hex(($data->amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
@@ -72,7 +72,7 @@ class SendLoop extends Command
                 'data' => $funcs.$real_to.$real_amount,
             ]]);
 
-            print_R($result);
+            // print_R($result);
             if(is_object($result)) {
 
                 if ($result->result != '') {
