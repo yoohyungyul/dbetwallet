a03b4599e5b9bc55309be871b501e3242fcc085e
유형열 <>
2019-01-14 14:29:58 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -59,7 +59,20 @@ class SendLoop extends Command
 
         foreach($history as $data) {
 
-            print_R($client);
+            
+            $real_to = str_pad(str_replace('0x','',$data->address_to), 64, '0', STR_PAD_LEFT);
+            $real_amount = str_pad($client->dec2hex(($data->amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
+
+
+            $result = $client->request('personal_unlockAccount', [$currency->address, $currency->password, '0x0a']);
+
+            print_R($result);
+            
+            // $result = $client->request('eth_sendTransaction', [[
+            //     'from' => $currency->address,
+            //     'to' => $currency->contract,
+            //     'data' => $funcs.$real_to.$real_amount,
+            // ]]);
 
             
             // try {
