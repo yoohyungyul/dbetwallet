48b0631abf21e39fea9a6f7db25d9877dbcc8018
유형열 <>
2019-01-15 14:14:53 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -41,8 +41,6 @@ class WalletController extends Controller
     public function getHistory() {
 
 
-        echo date('Ymd h:i:s');
-        exit;
 
         $currencyData = Currency::where('id', '=', env('CURRENCY_ID', '1'))->first();
         $balanceData = Balance::where('user_id',Auth::user()->id)->where('currency_id', '=', env('CURRENCY_ID', '1'))->first();
