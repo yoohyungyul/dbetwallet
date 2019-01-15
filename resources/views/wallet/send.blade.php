5e47db63159260e6f554495be9b9fd8d80813803
유형열 <>
2019-01-15 15:20:28 +0900

    no message

--- resources/views/wallet/send.blade.php
+++ resources/views/wallet/send.blade.php
@@ -51,47 +51,19 @@
                 </form>
 
 
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="input-group">
-                            <input id="scanner_input" class="form-control" placeholder="Click the button to scan an EAN..." type="text" /> 
-                            <span class="input-group-btn"> 
-                                <button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner">
-                                    <i class="fa fa-barcode"></i>
-                                </button> 
-                            </span>
-                        </div><!-- /input-group -->
-                    </div><!-- /.col-lg-6 -->
-                </div><!-- /.row -->
-                <div class="modal" id="livestream_scanner">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                <h4 class="modal-title">Barcode Scanner</h4>
-                            </div>
-                            <div class="modal-body" style="position: static">
-                                <div id="interactive" class="viewport"></div>
-                                <div class="error"></div>
-                            </div>
-                            <div class="modal-footer">
-                                <label class="btn btn-default pull-left">
-                                    <i class="fa fa-camera"></i> Use camera app
-                                    <input type="file" accept="image/*;capture=camera" capture="camera" class="hidden" />
-                                </label>
-                                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                            </div>
-                        </div><!-- /.modal-content -->
-                    </div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
+                
+
+
+                <!--input type=text size=16 placeholder="Tracking Code" class=qrcode-text><label class=qrcode-text-btn><input type=file accept="image/*" capture=environment onchange="openQRCamera(this);" tabindex=-1></label> 
+                <input type=button value="Go" disabled>
 
                 <style>
-	#interactive.viewport {position: relative; width: 100%; height: auto; overflow: hidden; text-align: center;}
-	#interactive.viewport > canvas, #interactive.viewport > video {max-width: 100%;width: 100%;}
-	canvas.drawing, canvas.drawingBuffer {position: absolute; left: 0; top: 0;}
-</style>
+                body, input {font-size:14pt}
+                input, label {vertical-align:middle}
+                .qrcode-text {padding-right:1.7em; margin-right:0}
+                .qrcode-text-btn {display:inline-block; background:url(//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/07/1499401426qr_icon.svg) 50% 50% no-repeat; height:1em; width:1.7em; margin-left:-1.7em; cursor:pointer}
+                .qrcode-text-btn > input[type=file] {position:absolute; overflow:hidden; width:1px; height:1px; opacity:0}
+                </style-->
 
 
             </div>
@@ -143,110 +115,22 @@
 
         return true;
     }
-
-
-    $(function() {
-	// Create the QuaggaJS config object for the live stream
-	var liveStreamConfig = {
-			inputStream: {
-				type : "LiveStream",
-				constraints: {
-					width: {min: 640},
-					height: {min: 480},
-					aspectRatio: {min: 1, max: 100},
-					facingMode: "environment" // or "user" for the front camera
-				}
-			},
-			locator: {
-				patchSize: "medium",
-				halfSample: true
-			},
-			numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
-			decoder: {
-				"readers":[
-					{"format":"ean_reader","config":{}}
-				]
-			},
-			locate: true
-		};
-	// The fallback to the file API requires a different inputStream option. 
-	// The rest is the same 
-	var fileConfig = $.extend(
-			{}, 
-			liveStreamConfig,
-			{
-				inputStream: {
-					size: 800
-				}
-			}
-		);
-	// Start the live stream scanner when the modal opens
-	$('#livestream_scanner').on('shown.bs.modal', function (e) {
-		Quagga.init(
-			liveStreamConfig, 
-			function(err) {
-				if (err) {
-					$('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
-					Quagga.stop();
-					return;
-				}
-				Quagga.start();
-			}
-		);
-    });
-	
-	// Make sure, QuaggaJS draws frames an lines around possible 
-	// barcodes on the live stream
-	Quagga.onProcessed(function(result) {
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay;
- 
-		if (result) {
-			if (result.boxes) {
-				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function (box) {
-					return box !== result.box;
-				}).forEach(function (box) {
-					Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-				});
-			}
- 
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-			}
- 
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-			}
-		}
-	});
-	
-	// Once a barcode had been read successfully, stop quagga and 
-	// close the modal after a second to let the user notice where 
-	// the barcode had actually been found.
-	Quagga.onDetected(function(result) {    		
-		if (result.codeResult.code){
-			$('#scanner_input').val(result.codeResult.code);
-			Quagga.stop();	
-			setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);			
-		}
-	});
-    
-	// Stop quagga in any case, when the modal is closed
-    $('#livestream_scanner').on('hide.bs.modal', function(){
-    	if (Quagga){
-    		Quagga.stop();	
-    	}
-    });
-	
-	// Call Quagga.decodeSingle() for every file selected in the 
-	// file input
-	$("#livestream_scanner input:file").on("change", function(e) {
-		if (e.target.files && e.target.files.length) {
-			Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
-		}
-	});
-});
-   
+    /*
+    function openQRCamera(node) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            node.value = "";
+            qrcode.callback = function(res) {
+            if(res instanceof Error) {
+                alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
+            } else {
+                node.parentNode.previousElementSibling.value = res;
+            }
+            };
+            qrcode.decode(reader.result);
+        };
+        reader.readAsDataURL(node.files[0]);
+    }
+    */
 </script>
 @endsection
