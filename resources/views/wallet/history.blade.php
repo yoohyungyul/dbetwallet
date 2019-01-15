7f8ca34fda08e27051c068573bf3b8a0df6a741d
유형열 <>
2019-01-15 15:52:05 +0900

    no message

--- resources/views/wallet/history.blade.php
+++ resources/views/wallet/history.blade.php
@@ -53,7 +53,6 @@
                                 <span class="btn btn-secondary btn-sm">pending</span>
                                 
                             @else
-                            <span class="btn btn-secondary btn-sm">pending</span>
                                 <span class="btn btn-success btn-sm">confirmed</span>
                                 <br>
                                 <a href="https://etherscan.io/tx/{{$item->data->txid}}" target="_blank">View transaction details</a>
