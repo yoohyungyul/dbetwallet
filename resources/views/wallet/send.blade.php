2d401a3f1d8318c3a7e13609073ba534820ab999
유형열 <>
2019-01-18 11:22:06 +0900

    no message

--- resources/views/wallet/send.blade.php
+++ resources/views/wallet/send.blade.php
@@ -32,7 +32,9 @@
         <div class="panel panel-default">
             <div class="panel-body">
 
-                <button class="btn" type="button" onclick="fnQrCode()">전송</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
 
 
@@ -72,13 +74,25 @@
 </div>
 
 
-<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="Modal" class="modal fade">
-  <div class="modal-dialog">
-	  <div class="modal-content">
-	  </div>
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
   </div>
 </div>
-
 
 @endsection
 
@@ -130,17 +144,17 @@
     }
 
 
-    function fnQrCode() {
+    // function fnQrCode() {
 
 
-        url = '/instascan';
+    //     url = '/instascan';
 
         
-        $("#Modal .modal-content").load(url, function() { 
-            $("#Modal").modal("show"); 
-        });
+    //     $("#Modal .modal-content").load(url, function() { 
+    //         $("#Modal").modal("show"); 
+    //     });
 
-    }
+    // }
 
 </script>
 @endsection
