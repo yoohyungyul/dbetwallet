1736c1f52ec8fa3ce3ad844ccbc325cf630cadc7
유형열 <>
2019-01-18 11:32:52 +0900

    no message

--- resources/views/wallet/send.blade.php
+++ resources/views/wallet/send.blade.php
@@ -10,8 +10,8 @@
 @section('content')
 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class="row  mt20">
     <div class="col-12">
