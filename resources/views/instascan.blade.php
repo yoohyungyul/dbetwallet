a8751ac047485db939882c6611c1f3fe03add6fc
유형열 <>
2019-01-18 14:17:27 +0900

    no message

--- resources/views/instascan.blade.php
+++ resources/views/instascan.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <title>Instascan</title>
-    <script type="text/javascript" src="/js/instascan_2.js"></script>
+    <script type="text/javascript" src="https://doublebet.net/js/instascan_2.js"></script>
   </head>
   <body>
     <video id="preview" style="width:100%;height:100%;"></video>
