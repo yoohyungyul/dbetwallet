7930deeb1dda8c532d6236f5af768379d8da01dc
유형열 <>
2019-01-18 14:15:23 +0900

    no message

--- resources/views/instascan.blade.php
+++ resources/views/instascan.blade.php
@@ -8,38 +8,40 @@
     <video id="preview" style="width:100%;height:100%;"></video>
     <script type="text/javascript">
     
-        let scanner = new Instascan.Scanner(
-            { 
-                video: document.getElementById('preview') 
-            }
-        );
-        scanner.addListener('scan', function (content) {
-            // alert(content);
-           opener.document.getElementById("address").value = content;
-           self.close();
+        // let scanner = new Instascan.Scanner(
+        //     { 
+        //         video: document.getElementById('preview') 
+        //     }
+        // );
+        // scanner.addListener('scan', function (content) {
+        //     // alert(content);
+        //    opener.document.getElementById("address").value = content;
+        //    self.close();
 
-
-        });
-        Instascan.Camera.getCameras().then( cameras => {
-            if (cameras.length > 0) {
-                scanner.start(cameras[1]);
-            } else {
-            alert('No cameras found.');
-            }
-        });
-
-        // document.addEventListener("DOMContentLoaded", event => {
-        // let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-        // Instascan.Camera.getCameras().then(cameras => {
-        //     scanner.camera = cameras[cameras.length - 1];
-        //     scanner.start();
-        // }).catch(e => console.error(e));
-
-        // scanner.addListener('scan', content => {
-        //     console.log(content);
-        // });
 
         // });
+        // Instascan.Camera.getCameras().then( cameras => {
+        //     if (cameras.length > 0) {
+        //         scanner.start(cameras[1]);
+        //     } else {
+        //     alert('No cameras found.');
+        //     }
+        // });
+
+        document.addEventListener("DOMContentLoaded", event => {
+        let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+        Instascan.Camera.getCameras().then(cameras => {
+            scanner.camera = cameras[cameras.length - 1];
+            scanner.start();
+        }).catch(e => console.error(e));
+
+        scanner.addListener('scan', content => {
+            // console.log(content);
+            opener.document.getElementById("address").value = content;
+            self.close();
+        });
+
+        });
   
     </script>   
   </body>
