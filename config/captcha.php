209211e996398baa54c6d6257ee528dc283c5514
유형열 <>
2019-01-28 17:43:26 +0900

    no message

--- config/captcha.php
+++ config/captcha.php
@@ -5,11 +5,10 @@ return [
     'characters' => '2346789abcdefghjmnpqrtuxyzABCDEFGHJMNPQRTUXYZ',
 
     'default'   => [
-        'length'    => 9,
+        'length'    => 5,
         'width'     => 120,
         'height'    => 36,
         'quality'   => 90,
-        'math'      => true,
     ],
 
     'flat'   => [
