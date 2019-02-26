939d8e0594c7aa8628e9a6fb532ee6597e60c951
유형열 <>
2019-02-26 16:59:51 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -25,8 +25,7 @@ class WalletController extends Controller
     // 지갑 
     public function getWallet() {
 
-        echo env('CURRENCY_ID', '1');
-        exit;
+        
 
         $currencyData = Currency::where('id', '=', env('CURRENCY_ID', '1'))->first();
         $walletData = Users_wallet::where('user_id',Auth::user()->id)->where('currency_id', '=', env('CURRENCY_ID', '1'))->first();
