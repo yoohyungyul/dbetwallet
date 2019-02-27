f666f0f1c42d41804ad570e7d30007da50bb5bd2
유형열 <>
2019-02-27 11:09:52 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -46,6 +46,9 @@ class SendLoop extends Command
     public function handle()
     {
 
+
+        echo env('CURRENCY_ID', '1');
+        exit;
         while (true) {
 
             echo "[" . date('Ymd h:i:s') . "] Work Start\n";
