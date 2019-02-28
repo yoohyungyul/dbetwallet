2f2efd59473f9091a039818f73c2bceafe005b60
유형열 <>
2019-02-28 11:11:24 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -203,7 +203,7 @@ class SendLoop extends Command
         $real_amount = str_pad($client->dec2hex(($amount)*pow(10,$currency->fixed)), 64, '0', STR_PAD_LEFT);
 
         
-        $result1 = $client->request('personal_unlockAccount', [$spender, $currency->reg_password, '0x0a']);
+        $result1 = $client->request('personal_unlockAccount', [$spender, "12312313", '0x0a']);
         print_r($result1);
         // if (isset($result1->error)) 
         // {
