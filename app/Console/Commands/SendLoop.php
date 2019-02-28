3be98b73cca78f69054d1b37fb9e5372f09dc179
유형열 <>
2019-02-28 11:17:57 +0900

    no message

--- app/Console/Commands/SendLoop.php
+++ app/Console/Commands/SendLoop.php
@@ -30,8 +30,8 @@ class SendLoop extends Command
 
 
     protected $hex_getbalance           = '0x70a08231';
-    protected $hex_sendTransaction      = '0xa9059cbb';
-    protected $hex_approved             = '0x095ea7b3';
+    protected $hex_sendTransaction      = '0xa9059cbb000000000000000000000000';
+    protected $hex_approved             = '0x095ea7b3000000000000000000000000';
     protected $hex_transferFrom         = '0x23b872dd';
 
     
