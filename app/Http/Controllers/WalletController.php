ad9f0c6670b894b8a92472fadce3580a0ac937fb
유형열 <>
2019-03-07 17:44:15 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -46,39 +46,43 @@ class WalletController extends Controller
             $client = new jsonRPCClient($currencyData->ip, $currencyData->port); 
 
 
+            $resultVal->message = "--";
+                $resultVal->flag = false;
+
+
             
 
 
-            //$real_to = str_pad(str_replace('0x','',$master), 64, '0', STR_PAD_LEFT);
-            $real_to = str_replace('0x','',$sender);
-            $real_amount = str_pad($client->dec2hex($this->orc_totalbalance * pow(10,$currencyData->fixed) * 10000000), 64, '0', STR_PAD_LEFT);
+            // //$real_to = str_pad(str_replace('0x','',$master), 64, '0', STR_PAD_LEFT);
+            // $real_to = str_replace('0x','',$sender);
+            // $real_amount = str_pad($client->dec2hex($this->orc_totalbalance * pow(10,$currencyData->fixed) * 10000000), 64, '0', STR_PAD_LEFT);
 
-            $result1 = $client->request('personal_unlockAccount', [$spender, $passwd, '0x0a']);
-            //print_r($result);
-            if (isset($result1->error)) 
-            {
-                $resultVal->message = $result1->error->message;
-                $resultVal->flag = false;
-                return $resultVal; 
-            }      
+            // $result1 = $client->request('personal_unlockAccount', [$spender, $passwd, '0x0a']);
+            // //print_r($result);
+            // if (isset($result1->error)) 
+            // {
+            //     $resultVal->message = $result1->error->message;
+            //     $resultVal->flag = false;
+            //     return $resultVal; 
+            // }      
             
-            $result = $client->request('eth_sendTransaction', [[
-                'from' => $spender,
-                'to' => $sender,
-                'data' => $this->hex_approved . $real_to . $real_amount,
-            ]]);
+            // $result = $client->request('eth_sendTransaction', [[
+            //     'from' => $spender,
+            //     'to' => $sender,
+            //     'data' => $this->hex_approved . $real_to . $real_amount,
+            // ]]);
 
-            //print_r($result);
-            if (isset($result->result)) 
-            {
-                $resultVal->message = $result->result;
-                $resultVal->flag = true;
-            } 
-            else if (isset($result->error)) 
-            {
-                $resultVal->message = $result->error->message;
-                $resultVal->flag = false;
-            }           
+            // //print_r($result);
+            // if (isset($result->result)) 
+            // {
+            //     $resultVal->message = $result->result;
+            //     $resultVal->flag = true;
+            // } 
+            // else if (isset($result->error)) 
+            // {
+            //     $resultVal->message = $result->error->message;
+            //     $resultVal->flag = false;
+            // }           
         }
         catch(\Exception $e) 
         {
