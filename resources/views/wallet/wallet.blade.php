2eadc6826160038819abf3e03ff465f559888b4e
유형열 <>
2019-03-13 19:10:57 +0900

    no message

--- resources/views/wallet/wallet.blade.php
+++ resources/views/wallet/wallet.blade.php
@@ -20,7 +20,7 @@
         <div class="tab-content " style="background:#fff;display: inline-block;width:100%;height:70px;padding-top:15px;padding-right:10px;">
             <!-- <span style="float: right;display: inline-block;padding:0 10px 0 10px">{{ $currency->label }}</span> -->
             <span style="float: right;">{{ number_format(  $balance->balance, $currency->fixed, ".", ",") }} {{$currency->label}}</span><br>
-            <span style="float: right;"><small>{{ number_format( ethBalance, $currency->fixed, ".", ",") }} ETH</small></span>
+            <span style="float: right;"><small>{{ number_format( ethBalance->balance, $currency->fixed, ".", ",") }} ETH</small></span>
        </div>
     </div>
 </div>
