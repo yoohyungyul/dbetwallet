176ebea0f9830d456b3d147a8fe05ea4d86e9421
유형열 <>
2019-03-13 19:03:16 +0900

    no message

--- app/Http/Controllers/Google2FAController.php
+++ app/Http/Controllers/Google2FAController.php
@@ -106,12 +106,29 @@ class Google2FAController extends Controller
                 $users_wallet->currency_id = env('CURRENCY_ID', '1');
                 $users_wallet->address = $address;
                 $users_wallet->push();
+
+                // 기본으로 이더리움 생성
+                $users_wallet = new Users_wallet;
+                $users_wallet->user_id = Auth::user()->id;
+                $users_wallet->currency_id = 3;
+                $users_wallet->address = $address;
+                $users_wallet->push();
+
+
         
                 // balance 생성
                 $balance = new Balance;
                 $balance->user_id = Auth::user()->id;
                 $balance->currency_id = env('CURRENCY_ID', '1');
                 $balance->push();
+
+                // 기본으로 이더리움 발란스 생성
+                $balance = new Balance;
+                $balance->user_id = Auth::user()->id;
+                $balance->currency_id = 3;
+                $balance->push();
+
+
 
             } catch (\Exception $e) {
                 DB::rollback();
