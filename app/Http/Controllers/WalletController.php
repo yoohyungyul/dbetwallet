1c286c00b778bba93509778379e5d88702b7a8ff
유형열 <>
2019-03-13 14:20:23 +0900

    no message

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -294,8 +294,9 @@ class WalletController extends Controller
         $s = $client->request('eth_getTransactionReceipt', [$txid]);
         $result = $client->request('eth_getTransactionByHash', [$txid]);
 
-        echo "blockNumber : ".$result->result->blockNumber."<br>";
-        print_R($result);
+        echo "blockNumber : ".hexdec($result->result->blockNumber)."<br>";
+        // print_R($result);
+
 
         exit;
 
