2b582d01c8b373a49a5c3119772568c0193dcede
유형열 <>
2019-03-14 13:57:19 +0900

    no message

--- app/Console/Commands/Deposit.php
+++ app/Console/Commands/Deposit.php
@@ -73,7 +73,11 @@ class Deposit extends Command {
                 Cache::forever('eth_last_deposit_block_'.$currency->id, $height);
 
                 try {
-                    echo $height;
+                    $result = $client->request('eth_getBlockByNumber', ['0x'.$client->dec2hex($height), true]);
+                    echo "  Block #".$height."...\n";
+                            
+
+                    
                     $height++;
                     echo "  Done!\n";
                 } catch(\Exception $e) {
