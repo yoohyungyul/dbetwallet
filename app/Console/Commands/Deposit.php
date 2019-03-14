1050e049c9bb51dfa7b3851f308e2c789a9ff8c1
유형열 <>
2019-03-14 14:21:34 +0900

    no message

--- app/Console/Commands/Deposit.php
+++ app/Console/Commands/Deposit.php
@@ -142,8 +142,6 @@ class Deposit extends Command {
                                 // }
                             }
                         }
-
-                        exit;
                     }
                             
 
