ebf4234064ead1e1a0705f2ab88a93521451d428
유형열 <>
2019-05-10 12:10:53 +0900

    no message

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,6 +3,6 @@
         <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"wallet") == "1") active @endif"    href="/wallet" >Wallet</a>
         <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"history") == "1") active @endif"    href="/history" >History</a>
         <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"send") == "1") active @endif"    href="/send" >Send</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1") active @endif"    href="/send" >Buy</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1") active @endif"    href="/buy" >Buy</a>
     </div>
 </nav>
