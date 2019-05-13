6bbd5d9ba55d72a824e5529eb1b1802d7c45cbcc
유형열 <>
2019-05-13 09:35:44 +0900

    no message

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,8 +1,8 @@
 <nav>
     <div class="nav nav-tabs nav-fill" >
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"wallet") == "1") active @endif"    href="/wallet" >Wallet</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"history") == "1") active @endif"    href="/history" >History</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"send") == "1") active @endif"    href="/send" >Send</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1") active @endif"    href="/buy" >Buy</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"wallet") == "1")    active @endif"  href="/wallet" >Wallet</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"history") == "1")   active @endif"  href="/history" >History</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"send") == "1")      active @endif"  href="/send" >Send</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1")       active @endif"  href="/buy" >Buy</a>
     </div>
 </nav>
