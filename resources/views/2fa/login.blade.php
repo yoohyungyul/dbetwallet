ae35c639763ab784e874dd2a5fbfa7237a6229bb
유형열 <>
2019-05-15 17:09:45 +0900

    no message

--- resources/views/2fa/login.blade.php
+++ resources/views/2fa/login.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body mt70">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/2fa/login') }}">
@@ -21,9 +21,9 @@
                         @endforeach
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email">
 
                                
@@ -34,20 +34,20 @@
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">OTP CODE</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="totp" type="text" class="form-control" name="totp" value="" maxlength="6">
 
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-6">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i> Login
+                                    <i class="fa fa-btn fa-user"></i> 로그인
                                 </button>
                             </div>
-                            <div class="col-md-12 text-right">
-                                <a href="/register">Register</a>
+                            <div class="col-md-6 text-right">
+                                <a href="/register" class="btn btn-light">가입하기</a>
                             </div>
                         </div>
                     </form>
