8e7afdc6f010e4656992479e7e718a50740c0f5d
유형열 <>
2019-05-15 15:58:53 +0900

    no message

--- app/Http/Controllers/Google2FAController.php
+++ app/Http/Controllers/Google2FAController.php
@@ -45,7 +45,7 @@ class Google2FAController extends Controller
         Session::put('2fa:store:key', $secret);
 
 
-        $imageDataUri = Google2FA::getQRCodeInline(env('APP_DOMAIN'), Auth::user()->email, $secret, 200);
+        $imageDataUri = Google2FA::getQRCodeGoogleUrl(env('APP_DOMAIN'), Auth::user()->email, $secret, 200);
         
 
         return view('2fa/enableTwoFactor', ['image' => $imageDataUri, 'secret' => $secret]);
