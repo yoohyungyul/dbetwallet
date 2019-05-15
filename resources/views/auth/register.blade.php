4c516d46b7fb5c19e6cce016ad100bfde553536d
유형열 <>
2019-05-15 16:57:47 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">이름</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
 
                                 @if ($errors->has('name'))
@@ -32,7 +32,7 @@
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -46,7 +46,7 @@
                         <div class="form-group">
                             <label for="recommender" class="col-md-4 control-label">추천인 코드</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="recommender" type="text" class="form-control" name="recommender" value="">
                                
                             </div>
