f145725f875a0b4dce45b38646b1f1e358de4fc5
유형열 <>
2019-05-17 15:58:24 +0900

    no message

--- resources/views/2fa/enableTwoFactor.blade.php
+++ resources/views/2fa/enableTwoFactor.blade.php
@@ -8,29 +8,29 @@
 			   <div class="col-lg-12" style="margin-top: 30px;">
 					<div class="panel panel-info">
 						<div class="panel-body text-center" style="margin-bottom:30px">
-                            <div class="h3"><span class="text-muted mr-3">Step 1.</span><span class="text-primary">Download the OTP app</span></div>
-                            <p>Download time-synchronized (OTP-certified) OTP-certified apps to your smartphone.</p>
+                            <div class="h3"><span class="text-muted mr-3">1단계 .</span><span class="text-primary">OTP 앱 다운로드</span></div>
+                            <p>시간 동기 (Time OTP)방식을 사용하는 OTP 인증 앱을 스마트폰에 다운로드합니다.</p>
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="row">
                                         <div class="col-md-6 text-center">
                                             <div class="border py-4 ">
-                                                <h3>Android</h3>
-                                                <p>Search in the Play Store.</p>
+                                                <h3>안드로이드</h3>
+                                                <p>플레이스토어에서 검색하세요.</p>
                                                 <div>
                                                     <img src="/img/otp_img2.jpg" alt="플레이스토어">
                                                 </div>
-                                                <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=ko" target="_blank" class="btn btn-success btn-sm">Play Store OTP</a>
+                                                <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=ko" target="_blank" class="btn btn-success btn-sm">플레이스토어 OT</a>
                                             </div>
                                         </div>
                                         <div class="col-md-6 text-center">
                                             <div class="border py-4 ">
-                                                <h3>iPhone</h3>
-                                                <p>Search in the App Store.</p>
+                                                <h3>아이폰</h3>
+                                                <p>앱스토어에서 검색하세요.</p>
                                                 <div>
                                                     <img src="/img/otp_img.jpg" alt="앱스토어">
                                                 </div>
-                                                <a href="https://itunes.apple.com/kr/app/google-authenticator/id388497605?mt=8" target="_blank" class="btn btn-info btn-sm">App Store OTP</a>
+                                                <a href="https://itunes.apple.com/kr/app/google-authenticator/id388497605?mt=8" target="_blank" class="btn btn-info btn-sm">앱스토어 OTP</a>
                                             </div>
                                         </div>
                                     </div>
@@ -39,24 +39,24 @@
                                     <div class="row">
                                         <div class="col-md-6 text-center">
                                             <img src="/img/googleotp01.jpg" class="img-responsive" style="width:100%">
-                                            <p class="mt-3">Google OTP App new users</p>
+                                            <p class="mt-3">Google OTP 앱  신규 사용자</p>
                                         </div>
                                         <div class="col-md-6 text-center">
                                             <img src="/img/googleotp02.jpg" class="img-responsive" style="width:100%">
-                                            <p class="mt-3">Google OTP App existing users</p>
+                                            <p class="mt-3">Google OTP 앱 기존 사용자</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <hr>
-                            <div class="h3"><span class="text-muted mr-3">Step 2.</span><span class="text-primary">QR code authentication</span></div>
-                            <p>Launch the 2-step verification mobile app and scan the QR code below:</p>
+                            <div class="h3"><span class="text-muted mr-3">Step 2.</span><span class="text-primary">OTP 코드 인증</span></div>
+                            <p>2단계. 인증 모바일 앱을 실행하여 아래의 QR코드를 스캔하세요:</p>
                             <div class="text-center">
                                 <img alt="Image of QR barcode" src="{{ $image }}" />
                             </div>
-                            <p>If your mobile app does not support QR code, please enter this code manually: <code>{{ $secret }}</code></p>
+                            <p>만약 모바일 앱이 QR코드를 지원하지 않는다면, 이 코드를 직접 입력하세요: <code>{{ $secret }}</code></p>
                             <hr/>
-                            <p>Enter the code displayed on the OTP after scanning the QR code.</p>
+                            <p>QR코드 스캔 후 OTP에 표시되는 코드를 입력하세요.</p>
                             <p><code>{{ env('APP_DOMAIN') }} ({{ Auth::user()->email }})</code></p>
                             <hr/>
                             @foreach ($errors->all() as $error)
@@ -79,7 +79,7 @@
                                 <div class="input-group {{ $errors->has('totp') ? ' has-error' : '' }}">
                                     <input  type="text" class="form-control" maxlength="6" name="totp" placeholder="Please enter OTP code">
                                     <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="submit"><span class="glyphicon glyphicon-lock"></span>certification</button>
+                                        <button class="btn btn-outline-secondary" type="submit"><span class="glyphicon glyphicon-lock"></span>인증</button>
                                     </div>
                                 </div>
                                 @if ($errors->has('totp'))
