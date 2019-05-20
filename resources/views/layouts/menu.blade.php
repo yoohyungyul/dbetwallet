f834a597e99979bc466207a4b62d6eade07914ff
유형열 <>
2019-05-20 13:30:19 +0900

    no message

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,8 +1,8 @@
 <nav>
     <div class="nav nav-tabs nav-fill" >
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"wallet") == "1")    active @endif"  href="/wallet" style="font-size:15px" >지갑</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"history") == "1")   active @endif"  href="/history" style="font-size:15px" >거래내역</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"send") == "1")      active @endif"  href="/send" style="font-size:15px" >보내기</a>
-        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1")       active @endif"  href="/buy" style="font-size:15px" >구매하기</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"wallet") == "1")    active @endif"  href="/wallet" style="font-size:13px" >지갑</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"history") == "1")   active @endif"  href="/history" style="font-size:13px" >거래내역</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"send") == "1")      active @endif"  href="/send" style="font-size:13px" >보내기</a>
+        <a class="nav-item nav-link @if(strpos($_SERVER["REQUEST_URI"],"buy") == "1")       active @endif"  href="/buy" style="font-size:13px" >구매하기</a>
     </div>
 </nav>
